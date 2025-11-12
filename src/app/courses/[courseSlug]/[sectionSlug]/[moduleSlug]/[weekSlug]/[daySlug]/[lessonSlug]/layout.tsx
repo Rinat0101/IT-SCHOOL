5f2db1bbf93dd2207cb2c7d1202e9be444c9182f
Anastasiia <anastasiaaaguseva@gmail.com>
2025-11-12 12:44:58 +0300

    layouts

---
+++ src/app/courses/[courseSlug]/[sectionSlug]/[moduleSlug]/[weekSlug]/[daySlug]/[lessonSlug]/layout.tsx
@@ -0,0 +1,8 @@
+// app/courses/[courseSlug]/[sectionSlug]/[moduleSlug]/[weekSlug]/[daySlug]/[lessonSlug]/layout.tsx
+export default function LessonLayout({
+    children,
+  }: {
+    children: React.ReactNode;
+  }) {
+    return <>{children}</>;
+  }
---
+++ src/app/courses/[courseSlug]/[sectionSlug]/layout.tsx
@@ -0,0 +1,8 @@
+// app/courses/[courseSlug]/[sectionSlug]/layout.tsx
+export default function SectionLayout({
+    children,
+  }: {
+    children: React.ReactNode;
+  }) {
+    return <>{children}</>;
+  }
